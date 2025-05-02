66a1786a7d6b0366a819708cf8edb581dbcf30d0
Onos <onosejoor14@gmail.com>
2025-05-02 18:10:42 +0100

    Add redirect in auth pages, when authorised

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,6 +1,13 @@
+import { verifySession } from "@/lib/actions/session";
 import SignInForm from "./_components/Form";
+import { redirect } from "next/navigation";
 
-export default function SigninPage() {
+export default async function SigninPage() {
+  const { isAuth } = await verifySession();
+
+  if (isAuth) {
+    redirect("/");
+  }
   return (
     <div className="sm:p-10">
       <SignInForm />
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
 import SignUpForm from "./_components/SignupForm";
+import { verifySession } from "@/lib/actions/session";
 
-export default function SigninPage() {
+export default async function SigninPage() {
+  const { isAuth } = await verifySession();
+
+  if (isAuth) {
+    redirect("/");
+  }
   return (
     <div className="sm:p-10">
       <SignUpForm />
