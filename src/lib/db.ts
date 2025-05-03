e4da482a7216bf1b4240d00c48358011fc66453f
Onos <onosejoor14@gmail.com>
2025-05-03 07:29:10 +0100

    Update db connection #2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -18,7 +18,7 @@ export default async function connectDB() {
   }
   if (!cached_connection.promise) {
     const opts = {
-      bufferCommands: false,
+      bufferCommands: true,
     };
     cached_connection.promise = mongoose
       .connect(MONGODB_URL, opts)
