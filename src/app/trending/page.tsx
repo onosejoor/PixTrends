8242825c28ae0e00c1e12ab853c9f7aaf4b5168a
Onos <onosejoor14@gmail.com>
2025-05-04 07:28:15 +0100

    Update code, add suspense boundary

--- src/app/trending/page.tsx
+++ src/app/trending/page.tsx
@@ -1,15 +1,23 @@
+import { Suspense } from "react";
 import { SearchBar } from "./_components/SearchBar";
 import TrendingPosts from "./_components/TrendingPosts";
 import { TrendingUsers } from "./_components/TrendingUsers";
+import TrendingUsersLoader from "@/components/loaders/TrendingUsersLoader";
+import PostLoader from "@/components/loaders/PostLoader";
 
 export default function TrendingPage() {
   return (
     <div className="grid gap-5 py-10">
       <SearchBar />
       <hr className="border-light-gray" />
-      <TrendingUsers />
+      <Suspense fallback={<TrendingUsersLoader />}>
+        <TrendingUsers />
+      </Suspense>
+
       <hr className="border-light-gray" />
-      <TrendingPosts />
+      <Suspense fallback={<PostLoader />}>
+        <TrendingPosts />
+      </Suspense>
     </div>
   );
 }
