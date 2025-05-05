7f45ca705c02d480e4a0feada2d4128513b7f3f9
Onos <onosejoor14@gmail.com>
2025-05-05 07:20:38 +0100

    update reent posts swr options

--- src/app/_components/RecentPosts.tsx
+++ src/app/_components/RecentPosts.tsx
@@ -8,7 +8,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import Link from "next/link";
 
 import { usePathname } from "next/navigation";
-import useSWR, { SWRConfiguration } from "swr";
+import useSWR from "swr";
 import RecentPostsError from "./error";
 import { SearchBar } from "../trending/_components/SearchBar";
 
@@ -22,10 +22,6 @@ type APIResponse = {
   success: true;
   data: IPost[];
 };
-
-const options : SWRConfiguration = {
-  revalidateIfStale: false
-}
 
 export function RecentPosts() {
   const path = usePathname();
@@ -48,7 +44,7 @@ export function RecentPosts() {
     isLoading,
     data: response,
     error,
-  } = useSWR<APIResponse>(url, fetcher, options);
+  } = useSWR<APIResponse>(url, fetcher);
 
   if (isProtectedRoute) {
     return;
